b2898a02e40e49b3480280ff6147e79e6b9bf382
Kirillov Alexander <saratovsource@gmail.com>
2012-05-30 01:49:09 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvamediaSite::Application.config.secret_token = '0d53796f14e82f5c94dac8b3523fac2130f00e85d8429416718cc2b8e0285ca2491233eb0f715c3cc7a62ed6df001ebc089188f7fd358456ebffbc17dbf15f4c'
