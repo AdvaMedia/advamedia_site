db8479b40100937d37d7d7b7c1fbd0789f86b7a3
Kirillov Alexander <saratovsource@gmail.com>
2012-05-30 17:42:58 +0400

    change main domain

--- config/initializers/locomotive.rb
+++ config/initializers/locomotive.rb
@@ -8,7 +8,7 @@ Locomotive.configure do |config|
   config.multi_sites do |multi_sites|
     # each new website you add will have a default entry based on a subdomain
     # and the multi_site_domain value (ex: website_1.locomotivehosting.com).
-    multi_sites.domain = 'advamedia.saratovsource.locum.ru' #'myhostingplatform.com'
+    multi_sites.domain = 'advamedia.ru' #'myhostingplatform.com'
 
     # define the reserved subdomains
     # Ex:
