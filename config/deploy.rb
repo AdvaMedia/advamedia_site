bb9bc1de08673c4c68d1487df964b2746808dede
Kirillov Alexander <saratovsource@gmail.com>
2013-09-27 00:14:53 +0400

    Revert rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ role :app,            deploy_server
 role :db,             deploy_server, :primary => true
 
 # Следующие строки необходимы, т.к. ваш проект использует rvm.
-set :rvm_ruby_string, "2.0.0"
+set :rvm_ruby_string, "1.9.3"
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 
